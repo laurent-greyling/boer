175bd1013c96a1c078db96f8a24ded6492be9b52
Laurent Greyling <l.greyling@niposoftware.com>
2017-03-24 13:23:20 +0100

    BoerCom signalR server/hub code

---
+++ BoerCom/Hubs/NotificationHub.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNet.SignalR;
+using Microsoft.AspNet.SignalR.Hubs;
+
+namespace BoerCom.Hubs
+{
+    [HubName("BoerHub")]
+    public class NotificationHub : Hub
+    {
+        public void NotifyAll(string name, string message)
+        {
+            Clients.All.broadCastAll(name, message);
+        }
+    }
+}
---
+++ BoerCom/StartUp.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Owin;
+using Microsoft.Owin.Cors;
+using Owin;
+
+[assembly: OwinStartup(typeof(BoerCom.StartUp))]
+namespace BoerCom
+{
+    public class StartUp
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.UseCors(CorsOptions.AllowAll);
+            app.MapSignalR();
+        }
+    }
+}
