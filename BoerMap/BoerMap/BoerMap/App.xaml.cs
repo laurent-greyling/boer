91489d7856425dfa6b275d37e0da7a782dad786f
Laurent Greyling <l.greyling@niposoftware.com>
2017-06-01 14:49:23 +0200

    Map stuff

---
+++ BoerMap/BoerMap/BoerMap/App.xaml.cs
@@ -0,0 +1,30 @@
+﻿using BoerMap.Shared;
+using Xamarin.Forms;
+
+namespace BoerMap
+{
+	public partial class App : Application
+	{
+		public App ()
+		{
+			InitializeComponent();
+
+			MainPage = new MapPage();//new MainPage();
+		}
+
+		protected override void OnStart ()
+		{
+			// Handle when your app starts
+		}
+
+		protected override void OnSleep ()
+		{
+			// Handle when your app sleeps
+		}
+
+		protected override void OnResume ()
+		{
+			// Handle when your app resumes
+		}
+	}
+}
---
+++ BoerMap/BoerMap/BoerMap/Shared/MapPage.cs
@@ -0,0 +1,48 @@
+﻿using Android.Locations;
+using Xamarin.Forms;
+using Xamarin.Forms.Maps;
+
+namespace BoerMap.Shared
+{
+    public class MapPage : ContentPage
+    {
+        public MapPage()
+        {
+            const string locationProvider = LocationManager.GpsProvider;
+            var location = new Location(locationProvider);
+            
+            var map = new Map(
+                MapSpan.FromCenterAndRadius(new Position(location.Latitude, location.Longitude), Distance.FromKilometers(0.3)))
+            {
+                IsShowingUser = true,
+                HeightRequest = 100,
+                WidthRequest = 960,
+                VerticalOptions = LayoutOptions.FillAndExpand,
+                MapType = MapType.Street
+            };
+
+            var stack = new StackLayout
+            {
+                Spacing = 0
+            };
+
+            //give this point the Latitude, Longitude of incoming message
+            var position = new Position(52.39, 4.66);
+            var pin = new Pin
+            {
+                Type = PinType.Place,
+                Position = position,
+                Label = "BoerMap"
+
+            };
+
+            map.Pins.Add(pin);
+
+            //TODO: map route to pin
+
+            stack.Children.Add(map);
+
+            Content = stack;
+        }
+    }
+}
